49be519dd163e87186736746e8a861e57ad8f206
guilherme santos <guilhermeroroni@gmail.com>
2023-06-21 12:33:38 -0300

    ajustes

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0"
 }
 
-# pagina_atual = [str("https://www.kabum.com.br/hardware/processadores?page_number={}&page_size=100&facet_filters=&sort=offer_price").format(i) for i in range(1, 9)]
+pagina_atual = [str("https://www.kabum.com.br/hardware/processadores?page_number={}&page_size=100&facet_filters=&sort=offer_price").format(i) for i in range(1, 9)]
 
-pagina_atual = [str("https://www.kabum.com.br/hardware/placa-de-video-vga?page_number={}&page_size=100&facet_filters=&sort=offer_price").format(i) for i in range(1, 15)]
+# pagina_atual = [str("https://www.kabum.com.br/hardware/placa-de-video-vga?page_number={}&page_size=100&facet_filters=&sort=offer_price").format(i) for i in range(1, 15)]
 
 product_link = []
 product_name = []
@@ -27,6 +27,9 @@ for link in pagina_atual:
 
     for product in products:
         product_path = (product.find('a'))['href']
+        match = re.search(r'(/produto/\d+)', product_path) #retornar apenas o numero dos produtos
+        if match:
+            product_path = match.group(1)
         product_link.append(f"https://www.kabum.com.br{product_path}")
 
         name = product.find(class_='nameCard')
@@ -34,15 +37,15 @@ for link in pagina_atual:
 
         price = product.find(class_='priceCard')
         price_text = price.text.strip()
-        price_digits = re.sub(r'[^0-9]', '', price_text)
 
-        if price_digits:
-            product_price.append(float(price_digits) / 100) # dividindo por 100 pra ajustar a representação em centavos
+        if price_text == "R$ ---":
+            product_price.append(None) # retorna nan caso o produto esteja esgotado (algo deu errado, revisar)
         else:
-            product_price.append(None)  # Adiciona None se não houver preço válido
+            product_price.append(price_text)  # Mantém o preço como string
 
 # Criando DataFrame
-df = pd.DataFrame({"Link": product_link, "Nome": product_name, "Preço": product_price})
+df = pd.DataFrame({"Link": product_link, "Nome": product_name, "Preço": product_price}, columns=["Link", "Nome", "Preço"])
 
 # CSV
-df.to_csv('placa_de_video2.csv')
+df.to_csv('processadores.csv')
+
